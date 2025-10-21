039b08ef41864616c574040e68269e14d2ecec4b
reapon66 <carlosdanielribeirodoo@gmail.com>
2025-10-21 17:25:10 -0300

    upp do projeto

---
+++ src/test/java/com/springlearn/SpringLearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearn.SpringLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
