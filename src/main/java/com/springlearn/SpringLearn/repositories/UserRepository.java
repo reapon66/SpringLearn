a0bb8da556bf0a44f0b06fa50dc69fb43b3b6794
reapon66 <carlosdanielribeirodoo@gmail.com>
2025-10-22 16:38:50 -0300

    auto preenchimento no banco de dados configuração de dependencias

---
+++ src/main/java/com/springlearn/SpringLearn/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springlearn.SpringLearn.repositories;
+
+import com.springlearn.SpringLearn.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
